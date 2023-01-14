d5db2283f651caba48d8753dd81a8a2110684120
Brian Zheng <65459060+brianzheng205@users.noreply.github.com>
2023-01-13 22:29:06 -0500

    Update player.py

--- engine-2023-master/my_bot/player.py
+++ engine-2023-master/my_bot/player.py
@@ -60,51 +60,6 @@ class Player(Bot):
 
         return False
     
-    def calc_strength(self, hole, iterations):
-        '''
-        
-        
-        '''
-
-        deck = eval7.Deck()
-        hole_card = [eval7.Card(card) for card in hole]
-
-        for card in hole_card:
-            deck.cards.remove(card)
-
-        score = 0
-
-        for _ in range(iterations):
-            deck.shuffle()
-
-            _COMM = 5
-            _OPP = 2
-
-            draw = deck.peek(_COMM + _OPP)
-
-            opp_hole = draw[:_OPP]
-            community = draw[_OPP:]
-
-            our_hand = hole_card +  community
-            opp_hand = opp_hole +  community
-
-            our_value = eval7.evaluate(our_hand)
-            opp_value = eval7.evaluate(opp_hand)
-
-            if our_value > opp_value:
-                score += 2
-            
-            elif our_value == opp_value:
-                score += 1
-
-            else:
-                score += 0
-
-        hand_strength = score / (2 * iterations)
-
-        return hand_strength
-
-
     def handle_new_round(self, game_state, round_state, active):
         '''
         Called when a new round starts. Called NUM_ROUNDS times.
@@ -175,59 +130,14 @@ class Player(Bot):
         
         if RaiseAction in legal_actions and self.strong_hole: # only consider raising if the hand we have is strong
             my_action = RaiseAction(max_raise)
-        elif RaiseAction in legal_actions and random.random() < 0.25: # sometimes randomly raise by twice the min raise
+        elif RaiseAction in legal_actions and random.random() < 0.2: # sometimes randomly raise by twice the min raise
             my_action = RaiseAction(min_raise * 2)
         elif RaiseAction in legal_actions and random.random() < 0.04: # let's go all in
             my_action = RaiseAction(max_raise)
+        elif CallAction in legal_actions:
+            my_action = CallAction()
         else:
-            pot_total = my_contribution + opp_contribution
-
-            if street < 3:
-                raise_amount = int(my_pip + continue_cost + 0.4 * (pot_total + continue_cost))
-            else:
-                raise_amount = int(my_pip + continue_cost + 0.75 * (pot_total + continue_cost))
-
-            raise_amount = max([min_raise, raise_amount])
-            raise_cost = raise_amount - my_pip
-
-            if (RaiseAction in legal_actions and (raise_cost <= my_stack)):
-                temp_action = RaiseAction(raise_amount)
-            elif (CallAction in legal_actions and (continue_cost <= my_stack)):
-                temp_action = CallAction()
-            elif CheckAction in legal_actions: 
-                temp_action = CheckAction()
-            else:
-                temp_action = FoldAction()
-
-            MONTE_CARLO_ITERS = 100
-            strength = self.calc_strength(my_cards, MONTE_CARLO_ITERS)
-
-            if continue_cost > 0:
-                scary = 0
-
-                if continue_cost > 6:
-                    scary = 0.15
-                if continue_cost > 12:
-                    scary = 0.25
-                if continue_cost > 50:
-                    scary = 0.35
-
-                strength = max([0, strength - scary])
-
-                pot_odds = continue_cost / (pot_total + continue_cost)
-
-                if strength > pot_odds:
-                    if random.random() < strength and strength > 0.5:
-                        my_action = temp_action
-                    else:
-                        my_action = CallAction()
-                else:
-                    my_action = FoldAction()
-            else:
-                if random.random() < strength:
-                    my_action = temp_action
-                else:
-                    my_action = CheckAction()
+            my_action = CheckAction()
 
         return my_action
 
